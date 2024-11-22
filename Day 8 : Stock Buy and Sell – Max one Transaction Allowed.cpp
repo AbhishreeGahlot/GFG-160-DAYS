efd0e6c70d366927aa8cce5e78e4fc6f8e475b71
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2024-11-22 21:28:35 +0530

    Create Day 8 : Stock Buy and Sell – Max one Transaction Allowed.cpp

---
+++ Day 8 : Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -0,0 +1,35 @@
+Stock Buy and Sell – Max one Transaction Allowed
+Difficulty: EasyAccuracy: 49.33%Submissions: 28K+Points: 2
+Given an array prices[] of length n, representing the prices of the stocks on different days. The task is to find the maximum profit possible by buying and selling the stocks on different days when at most one transaction is allowed. Here one transaction means 1 buy + 1 Sell. If it is not possible to make a profit then return 0.
+
+Note: Stock must be bought before being sold.
+
+Examples:
+
+Input: prices[] = [7, 10, 1, 3, 6, 9, 2]
+Output: 8
+Explanation: You can buy the stock on day 2 at price = 1 and sell it on day 5 at price = 9. Hence, the profit is 8.
+Input: prices[] = [7, 6, 4, 3, 1]
+Output: 0 
+Explanation: Here the prices are in decreasing order, hence if we buy any day then we cannot sell it at a greater price. Hence, the answer is 0.
+  class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int min_price = INT_MAX; 
+        int max_profit = 0; 
+        
+        for (int price : prices) {
+    
+            min_price = min(min_price, price);
+          
+            int profit = price - min_price;
+            
+            
+            max_profit = max(max_profit, profit);
+        }
+        
+        return max_profit; 
+    }
+    
+};
