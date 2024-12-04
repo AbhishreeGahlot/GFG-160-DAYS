704f785cf1ef657e9bc298caf230547871621714
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2024-12-04 20:34:26 +0530

    Update DAY 20 - Strings Rotations of Each Other.cpp

--- DAY 20 - Strings Rotations of Each Other.cpp
+++ DAY 20 - Strings Rotations of Each Other.cpp
@@ -18,3 +18,51 @@ Constraints:
 
   code:
   
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+        // Your code here
+        // Check if lengths are equal
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        // Concatenate s1 with itself
+        string temp = s1 + s1;
+        // Check if s2 is a substring of the concatenated string
+        return temp.find(s2) != string::npos; //checks if s2 IS A substring in temp or not 
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
